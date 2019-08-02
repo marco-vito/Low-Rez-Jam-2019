ba8bef3f477cb00acd24d5d9202bd2e059033696
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-02 07:03:47 +0200

    implement-random-walk
    As a way to get used to 2D Arrays, Paul asked me to develope a random walk function

--- Main Scene/MainScene.gd
+++ Main Scene/MainScene.gd
@@ -3,13 +3,13 @@ extends Node2D
 var height = 8
 var width = 8
 var tilesArray = []
+var posX = 0
+var posY = 0
+var dirArray = ["North", "South", "East", "West"]
 
 func _ready():
 	randomize()
-	print(tilesArray)
-	_set_array(tilesArray)
-	print(tilesArray)
-	_draw_tiles(tilesArray)
+	_drunkard_walk()
 
 func _set_array(array):
 	for x in range(width):
@@ -22,3 +22,23 @@ func _draw_tiles(array):
 	for x in range(width):
 		for y in range(height):
 			$TileMap.set_cell(x, y, array[x][y])
+
+func _drunkard_walk():
+	if posX > 8 or posY > 8:
+		return
+	if $TileMap.get_cell(posX, posY) != 2:
+		$TileMap.set_cell(posX, posY, 2)	
+	var direction = dirArray[randi()%dirArray.size()]
+	if direction == "North":
+		posY -= 1
+		abs(posY)
+	elif direction == "South":
+		posY += 1
+		abs(posY)
+	elif direction == "East":
+		posX += 1
+		abs(posX)
+	elif direction == "West":
+		posX -= 1
+		abs(posX)
+	_drunkard_walk()
