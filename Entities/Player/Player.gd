d716f1363f68a63b23828b3cda308329569147b1
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-05 12:17:51 +0200

    Added simple movement to player
    Added a simple movement loop to the player, which later can be expanded to include raycasts signals

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -1,13 +1,24 @@
 extends "res://Entities/Entity.gd"
 
-const SPEED = 200
+var moving = false
 
+func _ready():
+	set_raycasts()
+	
 func _input(event):
-	if event.is_action_pressed("ui_left"):
-		direction = Vector2(dirX[Directions.LEFT], dirY[Directions.LEFT])
-	elif event.is_action_pressed("ui_right"):
-		direction = Vector2(dirX[Directions.RIGHT], dirY[Directions.RIGHT])
-	elif event.is_action_pressed("ui_up"):
-		direction = Vector2(dirX[Directions.UP], dirY[Directions.UP])
-	elif event.is_action_pressed("ui_down"):
-		direction = Vector2(dirX[Directions.DOWN], dirY[Directions.DOWN])
+	if event:
+		if event.is_action_pressed("ui_left"):
+			direction = Vector2(dirX[Directions.LEFT], dirY[Directions.LEFT])
+		elif event.is_action_pressed("ui_right"):
+			direction = Vector2(dirX[Directions.RIGHT], dirY[Directions.RIGHT])
+		elif event.is_action_pressed("ui_up"):
+			direction = Vector2(dirX[Directions.UP], dirY[Directions.UP])
+		elif event.is_action_pressed("ui_down"):
+			direction = Vector2(dirX[Directions.DOWN], dirY[Directions.DOWN])
+		moving = true
+		_check_movement()
+		
+func _check_movement():
+	if moving:
+		move()
+		moving = false
