43d7112430a72a5719cf995edf4bc5a2a0ee582c
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-05 10:12:09 +0200

    Update LevelGeneration.gd
    Updated the LevelGeneration script. It contains bugs.

--- MainScene/LevelGeneration.gd
+++ MainScene/LevelGeneration.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 #Varibales to control the size of the level beign generated
-export var width = 0
-export var height = 0
+export var width = 24
+export var height = 24
 
 #Arrays to control the direction of the steps for the random generation of tile. [North, South, East, West].
 var dirX = [-1, 1, 0, 0]
@@ -18,11 +18,12 @@ func _ready():
 		print(row)
 
 func _generate_floor_map():
-	var map : Array = _inicialize_2D_array(width, height, Tiles.WALLS)
+	var map : Array = _initialize_2D_array(width, height, Tiles.WALLS)
 	map = _set_random_paths(map, randi()%width, randi()%height, 4, 3)
+	map = _set_random_rooms(map, randi()%width, randi()%height, 3, 2)
 	return map 
 
-func _inicialize_2D_array(sizeX, sizeY, defaultValue):
+func _initialize_2D_array(sizeX, sizeY, defaultValue):
 	var array = []
 	for x in range(sizeX):
 		array.append([])
@@ -39,8 +40,22 @@ func _set_random_paths(array, startX, startY, steps, paths):
 			steps -= 1
 		var r = randi()%dirX.size()
 		x = clamp(x+dirX[r], 0, array.size())
-		y = clamp(x+dirY[r], 0, array[x].size())
+		y = clamp(y+dirY[r], 0, array[x].size())
 	paths -= 1
 	if paths <= 0:
-		return
-	_set_random_paths(array, randi()%x, randi()%y, steps, paths)
+		return array
+	_set_random_paths(array, randi()%array.size(), randi()%array[x].size(), steps, paths)
+	
+func _set_random_rooms(array, startX, startY, maxSize, rooms):
+	var x = startX
+	var y = startY
+	var width = randi()%(maxSize-1)+1
+	var height = randi()%(maxSize-1)+1
+	for x in range(width):
+		for y in range(height):
+			if array[x][y] != Tiles.FLOOR:
+				array[x][y] = Tiles.FLOOR
+	rooms -= 1
+	if rooms <= 0:
+		return array
+	_set_random_rooms(array, startX, startY, maxSize, rooms)
