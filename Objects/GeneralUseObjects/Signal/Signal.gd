511722dc06fb3d672ae1c323447856d598caca71
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-06 12:53:08 +0200

    Created Signal object
    A glowing signal that will appear only in the dark, and when interacted with, will activated a machine.

--- Objects/GeneralUseObjects/Signal/Signal.gd
+++ Objects/GeneralUseObjects/Signal/Signal.gd
@@ -1,2 +1,18 @@
 extends "res://Objects/GeneralUseObjects/Interactable/Interactable.gd"
 
+signal used
+
+#Variable to control if the object is on the light or not
+var illuminated : bool = false setget _SetIlluminated
+
+#Function to control if the sign is visible or not. It should only be visible in the dark.
+func _SetIlluminated(parameter : bool):
+	illuminated = parameter
+	if parameter:
+		$Sign.visible = false
+	else:
+		$Sign.visible = true
+
+func _on_interact(trigger):
+	emit_signal("used")
+	queue_free()
--- Objects/Slate/Slate.gd
+++ Objects/Slate/Slate.gd
@@ -1,8 +1,5 @@
 extends "res://Objects/GeneralUseObjects/Interactable/Interactable.gd"
 
-#Variable to control if the object is on the light or not
-var illuminated : bool = false setget _SetIlluminated
-
 #Variable to control if the object is still a sign or if the Slate was already unborrowed
 var isSign = true
 
@@ -12,15 +9,6 @@ var interacting = true
 func _ready():
 	$CanvasLayer/RichTextLabel.visible = false
 	$Slate.texture.region.size.y = 0
-
-#Function to control if the sign is visible or not. It should only be visible in the dark.
-func _SetIlluminated(parameter : bool):
-	if isSign:
-		illuminated = parameter
-		if parameter:
-			$Sign.visible = false
-		else:
-			$Sign.visible = true
 
 #Function to unborrow the Slate and make the sign disappear
 func _setSlate():
