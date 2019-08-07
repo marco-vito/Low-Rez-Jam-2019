053cbc623c5511069d803afb68e29cc6547493b3
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-07 11:32:12 +0200

    Created Exit
    Created the Exit object, that'll lead the player to the next level

--- Objects/Exit/Exit.gd
+++ Objects/Exit/Exit.gd
@@ -1,4 +1,6 @@
 extends "res://Objects/GeneralUseObjects/Interactable/Interactable.gd"
 
+export (PackedScene) var nextLevel
+
 func _on_interact(trigger):
-	trigger.recharge_battery()
+	get_tree().change_scene(nextLevel)
