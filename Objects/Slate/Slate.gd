12da5abed60277a3edf0f0e0ed10e4f0874e94f8
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-07 10:38:33 +0200

    Reworked Slate
    Made the object more independent, since it does not need to have an instance of the Symbol

--- Objects/Slate/Slate.gd
+++ Objects/Slate/Slate.gd
@@ -4,22 +4,12 @@ extends "res://Objects/GeneralUseObjects/Interactable/Interactable.gd"
 var interactable = true
 
 func _ready():
-	set_process(false)
-	$PromptArea/CollisionShape2D.disabled = true
-	$Sprite/StaticBody2D/CollisionShape2D.disabled = true
 	$CanvasLayer/RichTextLabel.visible = false
 	$Sprite.texture.region.size.y = 0
-	$Signal.connect("used", self, "_setSlate")
 	
 func _process(delta):
 	if $Sprite.texture.region.size.y < 32:
 		$Sprite.texture.region.size.y += 1
-
-#Function to unborrow the Slate
-func _setSlate():
-	set_process(true)
-	$Sprite/StaticBody2D/CollisionShape2D.disabled = false
-	$PromptArea/CollisionShape2D.disabled = false
 
 func _on_interact(trigger):
 	if interactable:
