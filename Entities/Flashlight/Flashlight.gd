0f8b8f735f7440c89109b7a3dd51987d06cce88d
Paul T <toepfer@aidaorga.de>
2019-08-08 16:45:45 +0200

    Added 64x64 pixel rendering and reworked battery display

--- Entities/Flashlight/Flashlight.gd
+++ Entities/Flashlight/Flashlight.gd
@@ -5,12 +5,12 @@ onready var player = get_tree().get_nodes_in_group("player")[0]
 
 func _ready():
 	player.connect("recharged", self, "_recharge_battery")
-	$Timer.connect("timeout", self, "_depleate_battery")
+	$Timer.connect("timeout", self, "_deplete_battery")
 	$Timer.start()
 
 func _process(delta):
 	_change_direction()
-	$CanvasLayer/TextureProgress.value = battery
+	$CanvasLayer/BatteryDisplay.value = battery
 
 func _input(event):
 	if event.is_action_pressed("light"):
@@ -25,8 +25,12 @@ func _change_direction():
 	look_at(get_global_mouse_position())
 	rotate(deg2rad(90.0))
 
-func _depleate_battery():
+func _deplete_battery():
 	battery -= 1
+	var c = $CanvasLayer/BatteryDisplay.get("custom_styles/fg").get_bg_color()
+	c.r = 1 - $CanvasLayer/BatteryDisplay.value / $CanvasLayer/BatteryDisplay.max_value
+	c.g = $CanvasLayer/BatteryDisplay.value / $CanvasLayer/BatteryDisplay.max_value
+	$CanvasLayer/BatteryDisplay.get("custom_styles/fg").set_bg_color(c)
 	if battery <= 0:
 		visible = false
 		$Timer.stop()
