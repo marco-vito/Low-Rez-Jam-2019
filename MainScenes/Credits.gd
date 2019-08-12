ee313089b497ae61a6fb5b96f9b1db8e923e53ac
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-12 10:55:29 +0200

    Made placeholder final room

--- MainScenes/Credits.gd
+++ MainScenes/Credits.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _input(event):
-	if event.is_action_pressed("light"):
+	if event.is_action_pressed("pickaxe"):
 		Global.totalLevels = 3
 		get_tree().change_scene("res://Levels/Levels.tscn")
 	elif event.is_action_pressed("ui_cancel"):
