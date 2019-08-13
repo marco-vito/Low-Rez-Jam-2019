93f159c69de4d2007b2f30ab2d115939fe0c90ce
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-13 11:04:18 +0200

    Updated computer ending

--- Computer.gd
+++ Computer.gd
@@ -2,4 +2,6 @@ extends "res://Objects/GeneralUseObjects/Interactable/Interactable.gd"
 
 func _on_interact(trigger):
 	Global.credit = "computer"
+	get_tree().get_nodes_in_group("gateway")[0].queue_free()
+	yield(get_tree().create_timer(1.0), "timeout")
 	get_tree().change_scene("res://MainScenes/Credits.tscn")
--- Objects/Gateway/Gateway.gd
+++ Objects/Gateway/Gateway.gd
@@ -1,5 +1,8 @@
 extends "res://Objects/GeneralUseObjects/Interactable/Interactable.gd"
 
+func _ready():
+	add_to_group("gateway")
+
 func _on_interact(trigger):
 	Global.credit = "gateway"
 	get_tree().change_scene("res://MainScenes/Credits.tscn")
