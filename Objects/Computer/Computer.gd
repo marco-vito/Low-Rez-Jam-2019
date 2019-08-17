3fac85bdea4ecefc783b8a5532df7bbe9c735a69
Marco Vito Oddo <marcovito@id.uff.br>
2019-08-17 23:27:54 +0200

    Last sound effects in place

--- Objects/Computer/Computer.gd
+++ Objects/Computer/Computer.gd
@@ -1,6 +1,8 @@
 extends "res://Objects/GeneralUseObjects/Interactable/Interactable.gd"
 
 func _on_interact(trigger):
+	var sfx = preload("res://Objects/Computer/ComputerInteract.wav")
+	Global.audioController.play_sfx(sfx, -10)
 	get_tree().get_nodes_in_group("gateway")[0].queue_free()
 	yield(get_tree().create_timer(1.0), "timeout")
 	get_tree().change_scene("res://MainScenes/Credits.tscn")
